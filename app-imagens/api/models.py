fca0475e6b3c314d736001acd44dd3b75b1dbad9
Kauã Régis <kauarlc@hotmail.com>
2025-06-02 16:20:44 -0300

    feat: proteção com JWT

--- app-imagens/api/models.py
+++ app-imagens/api/models.py
@@ -1,1 +1,9 @@
 import mongoengine as me
+from datetime import datetime
+
+class Image(me.Document):
+    id = me.SequenceField(primary_key=True)
+    title = me.StringField(required=True, max_length=60)
+    description = me.StringField(max_length=250)
+    image = me.FileField(required=True)
+    created_at = me.DateTimeField(required=True, default=lambda: datetime.utcnow())
--- app-imagens/api/serializers.py
+++ app-imagens/api/serializers.py
@@ -1,2 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Image
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=60, required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+class ImageSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=60)
+    description = serializers.CharField(max_length=250, allow_blank=True)
+    image = serializers.FileField()
+    created_at = serializers.DateTimeField(read_only=True)
