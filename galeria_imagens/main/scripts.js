67678195a9bb3e9ba4d17090af469e4244b62799
Kauã Régis <kauarlc@hotmail.com>
2025-06-07 14:44:42 -0300

    feat: tratando erros e refatorando o JS

--- galeria_imagens/main/scripts.js
+++ galeria_imagens/main/scripts.js
@@ -2,33 +2,46 @@ let token;
 const TOKEN_URL = 'http://localhost:8000/token';
 const API_URL = 'http://localhost:8000/api/images/';
 
-axios.post(TOKEN_URL, {
-    username: 'admin',
-    password: 'admin123'
-})
-.then(response => {
-    token = response.data.access;
-    return axios.get(API_URL, {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    });
-})
-.then(response => {
-    const imagens = response.data;
+function criarElementoImagem(img) {
+    const li = document.createElement('li');
+    const image = document.createElement('img');
+    image.src = img.image;
+    image.alt = img.title || 'Imagem';
+    image.style.maxWidth = '100%';
+    li.appendChild(image);
+    return li;
+}
+
+function carregarImagens() {
     const ul = document.getElementById('imagens');
-    imagens.forEach(img => {
-        const li = document.createElement('li');
-        const image = document.createElement('img');
-        image.src = img.image;
-        image.alt = img.title || 'Imagem';
-        image.style.maxWidth = '100%';
-        li.appendChild(image);
-        ul.appendChild(li);
+    const loading = document.getElementById('loading');
+    loading.style.display = 'block';
+    ul.innerHTML = '';
+
+    axios.post(TOKEN_URL, {
+        username: 'admin',
+        password: 'admin123'
+    })
+    .then(res => {
+        token = res.data.access;
+        return axios.get(API_URL, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+    })
+    .then(res => {
+        ul.innerHTML = '';
+        res.data.forEach(img => {
+            ul.appendChild(criarElementoImagem(img));
+        });
+        loading.style.display = 'none';
+    })
+    .catch(err => {
+        ul.innerHTML = '<li>Erro ao carregar imagens.</li>';
+        loading.style.display = 'none';
+        console.error(err);
     });
-})
-.catch(error => {
-    const ul = document.getElementById('imagens');
-    ul.innerHTML = '<li>Erro ao carregar imagens.</li>';
-    console.error(error);
-});
+}
+
+carregarImagens();
