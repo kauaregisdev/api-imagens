5807d0f7030f42548253af54d8574d14aefd5901
Kauã Régis <kauarlc@hotmail.com>
2025-06-17 10:32:17 -0300

    atualizando o CORS

--- app_imagens/app_imagens/settings.py
+++ app_imagens/app_imagens/settings.py
@@ -31,8 +31,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
-# CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
 
 MONGODB_URI = os.environ.get('MONGODB_URI')
 if MONGODB_URI:
