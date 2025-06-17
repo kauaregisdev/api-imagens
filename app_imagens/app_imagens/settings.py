750b6faaac4b6ba70f19320ff2a930ef46ba4cec
Kauã Régis <kauarlc@hotmail.com>
2025-06-16 21:09:07 -0300

    corrigindo ALLOWED_HOSTS

--- app_imagens/app_imagens/settings.py
+++ app_imagens/app_imagens/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-insegura')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '')
 
