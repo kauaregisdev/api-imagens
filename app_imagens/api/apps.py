07a0f5d6c20b35c1a26ba82822fd51704d9bde4f
Kauã Régis <kauarlc@hotmail.com>
2025-06-17 08:08:48 -0300

    implementando criação de superuser

--- app_imagens/api/apps.py
+++ app_imagens/api/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        except OperationalError:
+            pass
